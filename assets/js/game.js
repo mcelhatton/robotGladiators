20960da1083fc3495d904487418fe38ad433a2ad
mcelhatton <colinm@mobiledealerdata.com>
2022-03-12 16:36:05 -0800

    saving progress

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,26 +1,35 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
+// check to see if the value of the playerHealth variable is greater than 0
+if (playerHealth > 0) {
+    console.log("Your player is still alive!");
+} else {
+    console.log("This will run instead.");
+}
+
 var playerAttack = 10;
 
 var enemyName = "Roborto";
-var enemyHealth = 50;
+var enemyHealth = 0;
 var enemyAttack = 12;
 
 var fight = function () {
     window.alert("Welcome to Robot Gladiators!");
 
-    // subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value of 'enemyHealth' variable
     enemyHealth = enemyHealth - playerAttack;
-    // Log a resulting message to the console so we know it worked
+    
     console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
-    // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value of 'playerHealth' variable
+
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    } else {
+        console.log(
+            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+        );
+    };
         playerHealth = playerHealth - enemyAttack;
-    // Log a resulting message to the console so we know it worked
-    console.log(
-        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-    );
-};
+    };
 
 fight();
